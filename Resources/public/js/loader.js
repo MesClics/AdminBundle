cbcbf1bde228a4b656c04549a417515f88e9e280
MesClics <mesclics@hotmail.com>
2018-10-26 18:00:00 +0200

    reformattage popup et loader pour affichage animé automatique même au submit du login

--- Resources/public/js/loader.js
+++ Resources/public/js/loader.js
@@ -1,8 +1,8 @@
 var loader = document.querySelector(".loader");
 var animationOn = false;
 
-//on vérifie s'il y a un form nouveau message
-var newMsg = document.querySelector(".message-form__d__new");
+// //on vérifie s'il y a un form nouveau message
+// var newMsg = document.querySelector(".message-form__d__new");
 
 // //on ajoute un eventlistener sur l'animation
 // newMsg.addEventListener('animationstart', function(e){
@@ -14,24 +14,43 @@ var newMsg = document.querySelector(".message-form__d__new");
 //     loaderClassAdder();
 // })
 
-// function loaderClassAdd() {
-//     loader.classList.add("loader-active");    
-// }
+function loaderClassAdd() {
+    loader.classList.add("loader-active");    
+}
 
-// function loaderClassRemove(){
-//     loader.classList.remove("loader-active");
-// }
+function loaderClassRemove(){
+    loader.addEventListener('animationiteration', function(e){
+        e.target.classList.remove("loader-active");
+    });
 
-// window.addEventListener('beforeunload', function(e){
-//     if(animationOn){
-//         return;
-//     }
-//     loaderClassAdd();
-// });
+    loader.addEventListener('webkitAnimationIteration', function (e) {
+        e.target.classList.remove("loader-active");
+    });
 
-// window.addEventListener('load', function (e) {
-//     console.log('début du load');
-//     if(loader.classList.contains("loader-active")){
-//         loaderClassRemove();
-//     }
-// });
+    loader.addEventListener('MSAnimationIteration', function (e) {
+        e.target.classList.remove("loader-active");
+    });
+    
+    
+    // loader.classList.remove("loader-active");
+}
+
+window.addEventListener('beforeunload', function(e){
+
+    if(animationOn){
+        return;
+    }
+
+    //check if popup is active
+    var popup = document.querySelector('.popup');
+    if(popup && !popup.classList.contains('closed')){
+        popup.classList.add('closed');
+    }
+    loaderClassAdd();
+});
+
+window.addEventListener('load', function (e) {
+    if(loader.classList.contains("loader-active")){
+        loaderClassRemove();
+    }
+});
