03b1be45561a719c33d783491903ed49745bcba6
MesClics <mesclics@hotmail.com>
2019-10-23 16:08:12 +0200

    IMPROVEMENTS : styles

--- Resources/public/js/compiled.js
+++ Resources/public/js/compiled.js
@@ -561,4 +561,30 @@ window.addEventListener("load", function(e){
         }
     })
 });
+window.addEventListener("load", function () {
+    var closeBtns = document.querySelectorAll(".oocss-close");
+    if (closeBtns.length) {
+        for (let i = 0; i < closeBtns.length; i++) {
+            let btn = closeBtns[i];
+
+            btn.addEventListener("click", function (event) {
+                event.preventDefault;
+                let cls = event.target.getAttribute("data-target-class");
+                btnContainer = findAncestor(event.target, cls);
+                //restart animations
+                event.target.animationPlayState = "running";
+                btnContainer.classList.add("oocss-closed");
+            });
+        }
+    }
+});
+window.addEventListener("load", function(){
+  let flashMessages = document.querySelectorAll('.flash-message');
+  if (flashMessages.length > 1) {
+    for (let i = 0; i < flashMessages.length; i++) {
+      let flashMessage = flashMessages[i];
+      flashMessage.style.order = i * -1;
+    }
+  }
+});
 //# sourceMappingURL=compiled.js.map
